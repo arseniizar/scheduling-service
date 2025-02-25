9f15fe0aaeffa34e61fb6c3df2efa6e7b1833818
arseniizar <arseniizarudniuk@gmail.com>
2025-02-25 01:58:46 +0100

    initial commit

---
+++ SchedulingService/Configuration/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SchedulingService.Models;
+
+namespace SchedulingService.Configuration;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions options) : base(options)
+    {
+    }
+    
+    public DbSet<ScheduledTask> ScheduledTasks { get; set; }
+}
---
+++ SchedulingService/Models/ScheduledTask.cs
@@ -0,0 +1,9 @@
+﻿namespace SchedulingService.Models;
+
+public class ScheduledTask
+{
+    public int Id { get; set; }
+    public required string TaskName { get; set; }
+    public DateTime ScheduledTime { get; set; }
+    public bool IsExecuted { get; set; }
+}
